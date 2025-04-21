5388809e8a82773333b557dc08629a1c80fe7200
Nihanth2025 <sk1441@gmail.com>
2025-04-21 16:18:37 +0530

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,9 @@
+print("Enter your age: ")
+age=int(input())
+if age>10:
+    if age>20:
+        print("You are allowed for class")
+    else:
+        print("You are allowed for class")
+else:
+    print("You are not allowed for class")
